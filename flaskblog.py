0b46ad4815e43dfc54b21ca855afe52cf7b58901
James McTighe <james.mctighe97@gmail.com>
2024-12-27 18:54:53 -0500

    added form html

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'a19b0a880bb9062865cd17e523db0ddf'
 
 posts = [
     {
@@ -26,6 +29,15 @@ def hello_world():
 def about():
     return render_template('about.html', title=about)
 
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title="Register", form=form)
+
+@app.route('/login')
+def register():
+    form = LoginForm()
+    return render_template('login.html', title="login", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
@@ -22,5 +22,7 @@ class LoginForm(FlaskForm):
                         validators=[DataRequired(), Email()])
     
     password = PasswordField('Password', validators=[DataRequired()])
+
+    remember = BooleanField('Remember Me')
     
     submit = SubmitField('Login')
