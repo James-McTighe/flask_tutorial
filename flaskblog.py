80147e6a3a32d347d70e3fccb0aa92aaff9fd44c
James McTighe <james.mctighe97@gmail.com>
2024-12-27 19:37:22 -0500

    added form feedback

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -22,20 +22,23 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello_world():
+def home():
     return render_template('home.html',posts=posts)
 
 @app.route("/about")
 def about():
     return render_template('about.html', title=about)
 
-@app.route('/register')
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title="Register", form=form)
 
-@app.route('/login')
-def register():
+@app.route("/login")
+def login():
     form = LoginForm()
     return render_template('login.html', title="login", form=form)
 
