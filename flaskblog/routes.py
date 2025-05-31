b4b57826f3a3e19a299bdec48223f2a3cba60f46
James McTighe <james.mctighe97@gmail.com>
2025-05-31 19:36:22 -0400

    worked on part 10

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -155,9 +155,28 @@ def user_post(username):
         
     return render_template('user_posts.html', posts=posts, user=user)
 
+def send_reset_email(user):
+    pass
+
 @app.route("/reset_password", methods=['GET','POST'])
 def reset_request():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password.', 'info')
+        return redirect(url_for('login'))
     return render_template('reset_request.html', title='Reset Password', form=form)
+
+@app.route("/reset_password<token>", methods=['GET','POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('That is an invalid or expired token', 'warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    return render_template('reset_token.html', title='Reset Password', form=form)
